3c0943eb7779591dc1c5589c82c287a263e27f4b
kstensing <stensingk@gmail.com>
2022-02-26 14:06:36 -0600

    added id test

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,9 +2,21 @@ const Employee = require('../lib/Employee');
 
 test("creates employee object", () => {
     const employee = new Employee('Arlo', "stensingk@gmail.com");
-    
+
 
     expect(employee.name).toBe('Arlo');
     expect(employee.id).toEqual(expect.any(Number));
     expect(employee.email).toBe("stensingk@gmail.com");
-})
+});
+
+test("get's employee's name", () => {
+    const employee = new Employee('Arlo', "stensingk@gmail.com");
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name.toString()));
+});
+
+test("get employee's id", () => {
+    const employee = new Employee('Arlo', "stensingk@gmail.com");
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id.toString()));
+});
