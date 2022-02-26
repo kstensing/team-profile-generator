048cf7a78fbbbb2ce68b98f554627e0b63559622
kstensing <stensingk@gmail.com>
2022-02-26 12:22:48 -0600

    passed employee test for properties

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -1,8 +1,10 @@
 const Employee = require('../lib/Employee');
 
 test("creates employee object", () => {
-    const employee = new Employee('Arlo');
+    const employee = new Employee('Arlo', "stensingk@gmail.com");
+    
 
     expect(employee.name).toBe('Arlo');
     expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toBe("stensingk@gmail.com");
 })
