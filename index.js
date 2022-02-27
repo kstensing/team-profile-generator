ea47d62eb01e5f71d300f6206f319cd001e989ae
kstensing <stensingk@gmail.com>
2022-02-27 17:17:14 -0600

    added questions for intern and engineer

--- index.js
+++ index.js
@@ -1,7 +1,203 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const createFile = require('./utils/generateHTML.js');
+// const createFile = require('./utils/generateHTML.js');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
+
+
+//console.log('Please build your team')
+
+const addManager = () => {
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'managerName',
+            message: "What is the team manager's name?",
+            validate: managerNameInput => {
+                if (managerNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is the team manager's id?",
+            validate: managerIdInput => {
+                if (managerIdInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an ID.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is the team manager's email?",
+            validate: managerEmailInput => {
+                if (managerEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an Email address.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOffice',
+            message: "What is the team manager's office number?",
+            validate: managerOfficeInput => {
+                if (managerOfficeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an office number.');
+                    return false;
+                }
+            }
+        },
+    ]);
+};
+//add employee
+
+const addEmployee = () => {
+    if (!teamData.employees) {
+        teamData.employees = [];
+    }
+return inquirer
+    .prompt([{
+        name: "employeeType",
+        type: "list",
+        message: "Which type of team member would you like to add?",
+        choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+    }, ])
+    .then(employeeData => {
+        teamData.employees.push(employeeData);
+        if 
+    });
+
+
+
+
+
+
+//engineer options
+inquirer
+    .prompt([{
+            type: 'input',
+            name: 'engineerName',
+            message: "What is your engineer's name?",
+            validate: engineerNameInput => {
+                if (engineerNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is your engineer's id?",
+            validate: engineerIdInput => {
+                if (engineerIdInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an ID.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is your engineer's email?",
+            validate: engineerEmailInput => {
+                if (engineerEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an Email address.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'engineerGithub',
+            message: "What is your engineer's GitHub username?",
+            validate: engineerGithubInput => {
+                if (engineerGithubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a GitHub username.');
+                    return false;
+                }
+            }
+        },
+    ])
+
+
+
+//intern options
+inquirer
+    .prompt([{
+            type: 'input',
+            name: 'internName',
+            message: "What is your intern's name?",
+            validate: internNameInput => {
+                if (internNameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a name.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is your intern's id?",
+            validate: internIdInput => {
+                if (internIdInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an ID.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is your intern's email?",
+            validate: internEmailInput => {
+                if (internEmailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter an Email address.');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'internSchool',
+            message: "What is your intern's school?",
+            validate: internSchoolInput => {
+                if (internSchoolInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a school.');
+                    return false;
+                }
+            }
+        },
+    ])
+
 
--- src/page-template.js
+++ src/page-template.js
@@ -1,3 +1,42 @@
+        
+const generateEmployee = employeesArr => {
+  return `
+  
+  <section class="my-3" id="portfolio">
+    <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+    <div class="flex-row justify-space-between">
+    ${projectsArr
+      .filter(({ feature }) => feature)
+      .map(({ name, description, languages, link }) => {
+        return `
+        <div class="col-12 mb-2 bg-dark text-light p-3">
+          <h3 class="portfolio-item-title text-light">${name}</h3>
+          <h5 class="portfolio-languages">
+            Built With:
+            ${languages.map(language => language).join(',')}
+          </h5>
+          <p>${description}</p>
+          <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+        </div>
+      `;
+        <section class="
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
+            header.github
+          }">GitHub</a>
+
+
+          <a href="https://github.com/${usernameLink}" target="_blank" rel="noopener noreferrer"></a>
+
+          <a href="mailto:stensingk@gmail.com" target="_blank" rel="noopener noreferrer">stensingk@gmail.com</a>
+
+
+    `
+}
+
+
+
+
+
 //creates the team information
 const generateTeam = teamArr => {
     return `
@@ -8,8 +47,7 @@ const generateTeam = teamArr => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Team Profiles</title>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-      <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
       <link rel="stylesheet" href="style.css">
     </head>
 
@@ -19,26 +57,17 @@ const generateTeam = teamArr => {
       <div class="container flex-row justify-space-between align-center py-3">
         <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
         <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${
-            header.github
-          }">GitHub</a>
-
-
-          <a href="https://github.com/${usernameLink}" target="_blank" rel="noopener noreferrer"></a>
-
-          <a href="mailto:stensingk@gmail.com" target="_blank" rel="noopener noreferrer">stensingk@gmail.com</a>
-
         </nav>
-      </div>
-    </header>
-    <main class="container my-5">
-      ${generateAbout(about)}
-      ${generateProjects(projects)}
-    </main>
-    <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
-    </footer>
-  </body>
-  </html>
-    `
-}
+        </div>
+      </header>
+      <main class="container my-5">
+        ${generateEmployee(employees)}
+        
+      </main>
+      <footer class="container text-center py-3">
+        <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.name}</h3>
+      </footer>
+    </body>
+    </html>
+        `
+}        
