934b50170141192faa760fb42dc23e496ab78097
kstensing <stensingk@gmail.com>
2022-02-26 21:26:38 -0600

    created require

--- app.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const createFile = require('./utils/generateHTML.js');
-
---
+++ index.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+const createFile = require('./utils/generateHTML.js');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
